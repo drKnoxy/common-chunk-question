62e91de1eeb927587d30086956bce1a3979088e1
Adam Knox <adam.w.knox@gmail.com>
2017-08-10 11:59:33 -0400

    issue still there

--- build/split-a.js
+++ build/split-a.js
@@ -1,1 +1,25 @@
-webpackJsonp([1],[,function(n,t,e){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var u=e(3);t.default=function(){Object(u.a)()}},,function(n,t,e){"use strict";function u(){return"function one included"}function c(){return"function two included"}t.a=u,t.b=c}]);
+webpackJsonp(
+  [1],
+  [
+    ,
+    function(n, t, e) {
+      'use strict';
+      Object.defineProperty(t, '__esModule', { value: !0 });
+      var u = e(3);
+      t.default = function() {
+        Object(u.a)();
+      };
+    },
+    ,
+    function(n, t, e) {
+      'use strict';
+      function u() {
+        return 'function one included';
+      }
+      function c() {
+        return 'function two included';
+      }
+      (t.a = u), (t.b = c);
+    }
+  ]
+);
--- build/split-b.js
+++ build/split-b.js
@@ -1,1 +1,25 @@
-webpackJsonp([0],[,,function(n,t,e){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var u=e(3);t.default=function(){Object(u.b)()}},function(n,t,e){"use strict";function u(){return"function one included"}function c(){return"function two included"}t.a=u,t.b=c}]);
+webpackJsonp(
+  [0],
+  [
+    ,
+    ,
+    function(n, t, e) {
+      'use strict';
+      Object.defineProperty(t, '__esModule', { value: !0 });
+      var u = e(3);
+      t.default = function() {
+        Object(u.b)();
+      };
+    },
+    function(n, t, e) {
+      'use strict';
+      function u() {
+        return 'function one included';
+      }
+      function c() {
+        return 'function two included';
+      }
+      (t.a = u), (t.b = c);
+    }
+  ]
+);
